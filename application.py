fb868582e2d7ad5d7ced2b01ae0393738439932e
Lucia0308 <lucia.leralta0308@usal.es>
2024-11-26 01:00:02 +0100

    Add files via upload

--- application.py
+++ application.py
@@ -1,4 +1,18 @@
-from capp import application
+from flask import Flask, render_template
+application = Flask(__name__)
+
+@application.route('/')
+@application.route('/home')
+def home():
+  return render_template('home.html')
+
+@application.route('/business')
+def business():
+  return render_template('business.html', title='business')
+
+@application.route('/ERASMUSAL')
+def ERASMUSAL():
+    return render_template('ERASMUSAL.html', title='ERASMUSAL')
 
 if __name__=='__main__':
   application.run(debug=True)  
